4028a4a46554aa7ce79ba89dbc52c571c04e419d
Takeru Ohta <phjgt308@gmail.com>
2023-08-18 21:12:29 +0900

    Update clap to v4

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 extern crate clap;
-extern crate jaeger_remote_tracer;
 extern crate fibers;
 extern crate futures;
 extern crate htrpc;
+extern crate jaeger_remote_tracer;
 extern crate rustracing;
 extern crate rustracing_jaeger;
 #[macro_use]
 extern crate trackable;
 
-use clap::{App, Arg};
-use fibers::{Spawn, Executor, ThreadPoolExecutor};
+use clap::{Arg, ArgAction, Command};
+use fibers::{Executor, Spawn, ThreadPoolExecutor};
 use futures::Future;
 use jaeger_remote_tracer::service::Service;
 use trackable::error::Failure;
 
 fn main() {
-    let matches = App::new("jaeger_remote_tracer")
+    let matches = Command::new("jaeger_remote_tracer")
         .arg(
-            Arg::with_name("SERVICE_NAME")
+            Arg::new("SERVICE_NAME")
                 .long("service")
-                .takes_value(true)
+                .action(ArgAction::Set)
                 .default_value("jaeger_remote_trace"),
         )
         .arg(
-            Arg::with_name("HTTP_PORT")
+            Arg::new("HTTP_PORT")
                 .long("http_port")
-                .short("p")
-                .takes_value(true)
+                .short('p')
+                .action(ArgAction::Set)
                 .default_value("8888"),
         )
         .get_matches();
-    let service_name = matches.value_of("SERVICE_NAME").unwrap();
-    let http_port = matches.value_of("HTTP_PORT").unwrap();
+    let service_name = matches.get_one::<String>("SERVICE_NAME").unwrap();
+    let http_port = matches.get_one::<String>("HTTP_PORT").unwrap();
 
     let bind_addr = format!("0.0.0.0:{}", http_port).parse().unwrap();
     let mut rpc_server = htrpc::RpcServerBuilder::new(bind_addr);
@@ -45,9 +45,11 @@ fn main() {
     );
 
     let executor = ThreadPoolExecutor::new().unwrap();
-    executor.spawn(rpc_server.start(executor.handle()).map_err(
-        |e| panic!("{}", e),
-    ));
+    executor.spawn(
+        rpc_server
+            .start(executor.handle())
+            .map_err(|e| panic!("{}", e)),
+    );
     executor.spawn(service.map_err(|e| panic!("{}", e)));
     track_try_unwrap!(executor.run().map_err(Failure::from_error));
 }
